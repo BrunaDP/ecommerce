8fcc15a0ca560fcba5899261c115e1709296d7e3
BrunaDP <florenza542@gmail.com>
2022-11-05 23:41:15 -0300

    subiendo cosas

---
+++ Downloads/ecommerce/pages/_app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import "../styles/globals"
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}    
---
+++ Downloads/ecommerce/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Product, FooterBanner, HeroBanner } from '../components';
+const Home = () => {
+    return (
+        <>
+            <div>
+                <HeroBanner />
+                <div className='p'>
+                    <a>Best Seller Products</a>
+                    <p>Shoes</p>
+                </div>
+            </div>
+            <div className='products-container'>
+                {['Product 1', "Product 2"].map((product) => product)}
+            </div>
+            <FooterBanner />
+        </>
+    )
+}
+
+export default Home
