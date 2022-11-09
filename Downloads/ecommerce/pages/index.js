1d7833b39c408a89cb81b621ea0af217a9608443
BrunaDP <florenza542@gmail.com>
2022-11-08 21:03:42 -0300

    subiendo cambios

--- Downloads/ecommerce/pages/index.js
+++ Downloads/ecommerce/pages/index.js
@@ -2,19 +2,20 @@ import React from 'react'
 import { Product, FooterBanner, HeroBanner } from '../components';
 const Home = () => {
     return (
-        <>
+        <div>
             <div>
                 <HeroBanner />
-                <div className='p'>
-                    <a>Best Seller Products</a>
+                <div className='products-heading'>
+                    <h2>Best Seller Products</h2>
                     <p>teste</p>
                 </div>
             </div>
+
             <div className='products-container'>
                 {['Product 1', "Product 2"].map((product) => product)}
             </div>
             <FooterBanner />
-        </>
+        </div>
     )
 }
 
