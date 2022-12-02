f28673011a444e9a5be16d741c694cb9519ef4a6
BrunaDP <florenza542@gmail.com>
2022-12-01 21:05:42 -0300

    subiendo actualizaciones

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -6,10 +6,31 @@ import toast from 'react-hot-toast';
 
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
+import getStripe from '../lib/getStripe';
 
 const Cart = () => {
     const cartRef = useRef();
-    const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext();
+    const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
+
+    const handleCheckout = async () => {
+        const stripe = await getStripe();
+
+        const response = await fetch('/api/stripe', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(cartItems),
+        });
+
+        if (response.statusCode === 500) return;
+
+        const data = await response.json();
+
+        toast.loading('Redirecting...');
+
+        stripe.redirectToCheckout({ sessionId: data.id });
+    }
 
     return (
         <div className="cart-wrapper" ref={cartRef}>
@@ -51,17 +72,17 @@ const Cart = () => {
                                 <div className="flex bottom">
                                     <div>
                                         <p className="quantity-desc">
-                                            <span className="minus" onClick="">
+                                            <span className="minus" onClick={() => toggleCartItemQuanitity(item._id, 'dec')}>
                                                 <AiOutlineMinus />
                                             </span>
-                                            <span className="num" onClick="">0</span>
-                                            <span className="plus" onClick=""><AiOutlinePlus /></span>
+                                            <span className="num">{item.quantity}</span>
+                                            <span className="plus" onClick={() => toggleCartItemQuanitity(item._id, 'inc')}><AiOutlinePlus /></span>
                                         </p>
                                     </div>
                                     <button
                                         type="button"
                                         className="remove-item"
-                                        onClick=""
+                                        onClick={() => onRemove(item)}
                                     >
                                         <TiDeleteOutline />
                                     </button>
